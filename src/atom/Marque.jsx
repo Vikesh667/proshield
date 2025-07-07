968df33f78a0e617962814f44b8a9cb11837029e
Vikesh <vik19sep@gmail.com>
2025-07-07 19:23:26 +0530

    marque component

---
+++ src/atom/Marque.jsx
@@ -0,0 +1,10 @@
+const Marque = () => {
+  return (
+    <div className="w-full flex gap-5">
+      <h1>Welcome Cyber Security</h1>
+      <h1>Welcome Cyber Security</h1>
+      <h1>Welcome Cyber Security</h1>
+    </div>
+  );
+};
+export default Marque;
